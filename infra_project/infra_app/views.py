dc7e945df91c3bee632682fdc1cc2a53f313e3d2
Not-user-1984 <deman.tmb.68@gmail.com>
2023-02-03 22:21:08 +0300

    новый деплой

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('<h1>hello is Docker</h1>')
 
 
 def second_page(request):
